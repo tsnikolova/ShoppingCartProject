0d196409b1270407383efc3d95f790c66fec863c
tsnikolova <cvetelina94@gmail.com>
2015-09-29 17:58:18 +0300

    few updates and new files

--- ShoppingCart/Views/html/startHtml.php
+++ ShoppingCart/Views/html/startHtml.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <title>Shopping Cart</title>
-    <link rel="stylesheet" href="../../../bootstrap/css/bootstrap.css">
-    <link rel="stylesheet" href="../../../bootstrap/css/bootstrap-theame2.min.css">
-    <!--<link rel="stylesheet" href="/css/styles.css"> -->
-    <script src="../../../bootstrap/js/jquery-2.1.1.min.js" defer></script>
-    <script src="../../../bootstrap/js/bootstrap.js" defer></script>
-    <!--<script src="/js/scripts.js" defer></script>-->
+    <link rel="stylesheet" href="../../Public/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="../../Public/bootstrap/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="../../Public/css/style.css">
+    <script src="../../Public/bootstrap/js/jquery-2.1.1.min.js" defer></script>
+    <script src="../../Public/bootstrap/js/bootstrap.js" defer></script>
+    <script src="../../Public/js/scripts.js" defer></script>
 </head>
 <body>
