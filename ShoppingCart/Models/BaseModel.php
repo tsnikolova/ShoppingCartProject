9e6825478a4172628d88a65554fc4a4497d0398e
tsnikolova <cvetelina94@gmail.com>
2015-09-29 17:43:55 +0300

    base model and user

---
+++ ShoppingCart/Models/BaseModel.php
@@ -0,0 +1,14 @@
+<?php
+namespace Models;
+use GFramework\Validation;
+abstract class BaseModel
+{
+    /**
+     * @var Validation
+     */
+    public  $validator;
+    public function __construct()
+    {
+        $this->validator = new Validation();
+    }
+}
---
+++ ShoppingCart/Models/User.php
@@ -0,0 +1,39 @@
+<?php
+namespace Models;
+use GFramework\ValidationRules;
+use Constants\Codes;
+class User extends BaseModel
+{
+    public $id;
+    public  $username;
+    public $password;
+    public $confirmPassword;
+    public $email;
+    public $isAdmin = 0;
+    public $isEditor = 0;
+    public function __construct($user = array())
+    {
+        parent::__construct();
+        if (is_array($user)) {
+            $this->id = $user['id'];
+            $this->username = $user['username'];
+            $this->password = $user['password'];
+            $this->confirmPassword = $user['confirmPassword'];
+            $this->email = $user['email'];
+            $this->isAdmin = $user['is_admin'];
+            $this->isAdmin = $user['is_editor'];
+        } else {
+            throw new \Exception("Invalid user's data'", 500);
+        }
+    }
+    public function validateUserData()
+    {
+        return $this->validator
+            ->setRules(ValidationRules::ALPHA_NUM_DASH, $this->username, null, Codes::USERNAME . Codes::INVALID)
+            ->setRules(ValidationRules::MIN_LENGTH, $this->username, 3, Codes::USERNAME . Codes::MIN_LENGTH)
+            ->setRules(ValidationRules::MIN_LENGTH, $this->password, 5, Codes::PASSWORD . Codes::MIN_LENGTH)
+            ->setRules(ValidationRules::MATCHES, $this->password, $this->confirmPassword, Codes::PASSWORD . Codes::NOT_MATCH)
+            ->setRules(ValidationRules::EMAIL, $this->email, null, Codes::EMAIL . Codes::INVALID)
+            ->validate();
+    }
+}
