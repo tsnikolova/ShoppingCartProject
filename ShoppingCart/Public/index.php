e9c6a092e0a0ec4fce04ae36bd86a2d4f221ab99
tsnikolova <cvetelina94@gmail.com>
2015-09-30 16:10:16 +0300

    updates

--- ShoppingCart/Public/index.php
+++ ShoppingCart/Public/index.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Tsvetelina
+ * Date: 29.9.2015 ã.
+ * Time: 18:02 ÷.
+ */
+
 error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
 include '../../Framework/App.php';
 $app = GFramework\App::getInstance();
